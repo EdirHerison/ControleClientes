97b40969d7422e755483b6e14faf1b12b0c99084
Edir Herison <edir_h@hotmail.com>
2022-10-28 13:47:52 -0300

    Criando tela principal

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,14 +12,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
+			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Controle de Clientes Rommanel");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		
 	}
 	
 	public static void main(String[] args) {
